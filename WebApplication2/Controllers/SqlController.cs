2de673d6301c53190b98dbf46150e62140f4323d
Kadir <kadiryapici60@gmail.com>
2017-08-22 15:56:33 +0300

    initialize

---
+++ WebApplication2/Controllers/SqlController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class SqlController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+
+            var conn = new SqlConnection();
+            return View();
+        }
+    }
+}
